9b4fdeafbad11cf60220f07a5261d6bc877e88e9
Lauri Haavisto <lauri.haavisto@pinja.com>
2026-04-10 11:53:21 +0300

    cleanup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from 'src/app/app.routes';
 import { JsonDateInterceptor } from 'src/app/shared/interceptors';
@@ -8,7 +7,6 @@ import { JsonDateInterceptor } from 'src/app/shared/interceptors';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
-    importProvidersFrom(BrowserModule),
     { provide: HTTP_INTERCEPTORS, useClass: JsonDateInterceptor, multi: true },
     provideHttpClient(withInterceptorsFromDi()),
   ],
